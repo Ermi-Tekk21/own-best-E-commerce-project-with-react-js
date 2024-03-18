ad6088868c338250c2b5d3f53c6b1ca0aa42c1af
Ermias-TekileMarkos <ermaatekk@gmail.com>
2024-03-18 22:29:39 +0300

    signIn modification

--- src/pages/signInPage/SignInPage.jsx
+++ src/pages/signInPage/SignInPage.jsx
@@ -146,17 +146,17 @@ function SignIn() {
                 </div>
               </div>
               <Link
-                href="/log-in"
+                to="/log-in"
                 className="font-medium text-blue-600 hover:text-blue-500"
               >
-                i already have accoun.
+                i already have an account.
               </Link>
               <div>
                 <button
                   type="submit"
                   className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                 >
-                  Create Account
+                  Create Account.
                 </button>
               </div>
             </form>
